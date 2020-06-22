8eba9906140a3ddc163013e08c5f0fc9f1c97e28
carlossanz <carsdgp@gmail.com>
2020-06-22 21:39:54 +0100

    tampoco

--- main.py
+++ main.py
@@ -29,3 +29,34 @@ data = pd.read_csv('OUTPUT/data.csv')
 
 # ojo. De Ras. si la api te devuelve una foto, mira cómo te la devuelve (será un array de numpy seguramente) 
 # y es fácil pasarla a imagen igual necesitas la librería pillow (ahí te dejo una pista)=
+                        
+
+#ejemplo de David. 
+'''
+import pandas as pd
+import matplotlib.pyplot as plt
+from argparse import ArgumentParser
+import sys, os, glob, json, requests
+import src.main_functions as fn
+
+
+parser = ArgumentParser(description="This program returns a small report of the top song of your favorite band,\
+\measuring the speechiness, the danceability and an index to measure how good is this song for burning calories dancing to it :)")
+
+parser.add_argument("--band",help="which band to look for", default=None)
+parser.add_argument("--bygenre", type=bool,  help="Do you want to analyze it comparing it to its genre? yes/no", default= 'no')
+parser.add_argument("--pdf", type=bool, help="Do you want to generate a pdf from the report? yes/no", default= 'no')
+
+args = parser.parse_args()
+
+### Data:   
+data = pd.read_csv('OUTPUT/data.csv')
+# read file
+with open('OUTPUT/artists_json.json', 'r') as myfile:
+    string=myfile.read()
+string_json = json.loads(string)
+dict_json = json.loads(string_json)
+
+if __name__=='__main__':
+    fn.main_function(data=data, dict_json=dict_json, band=args.band, bygenre=args.bygenre, pdf=args.pdf) 
+'''
